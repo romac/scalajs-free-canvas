71cf48aab79182b113db428b986a1500e4f88b89
Romain Ruetschi <romain.ruetschi@gmail.com>
2016-04-08 16:53:30 +0200

    Cluster points together.

--- src/test/scala/me/romac/freecanvas/Main.scala
+++ src/test/scala/me/romac/freecanvas/Main.scala
@@ -35,14 +35,43 @@ object Main extends JSApp {
   val rnd = new Random()
   val colors = Vector(Red, Blue, Green)
 
-  val randomPoints = (0 to 1000) map { i =>
-    Point("", rnd.nextInt(160), rnd.nextInt(160), colors(rnd.nextInt(3)))
+  val MAX   = 600
+  val SHIFT = 400
+
+  // Box-muller transformer
+  def genGaussian(n: Int, scale: Double): List[(Int, Int)] = {
+    (0 until n).toList map { i =>
+      val (u1, u2) = (rnd.nextDouble, rnd.nextDouble)
+
+      val z0 = Math.sqrt(-2 * Math.log(u1)) * Math.cos(2 * Math.PI * u2) / 3
+      val z1 = Math.sqrt(-2 * Math.log(u1)) * Math.sin(2 * Math.PI * u2) / 3
+
+      ((z0 * scale).toInt, (z1 * scale).toInt)
+    }
   }
 
+  val clusters: List[List[(Int, Int)]] =
+    (0 to 2).toList map { _ =>
+      genGaussian(1000, 100).map { case (x, y) =>
+        (x + MAX/2, y + MAX/2)
+      }
+  }
+
+  val randomPoints =
+    clusters flatMap { (c: List[(Int, Int)]) =>
+      val List((shiftX, shiftY)) = genGaussian(1, SHIFT)
+
+      println((shiftX, shiftY))
+
+      c map { case (x, y) =>
+        Point("", x + shiftX, y + shiftY, colors(rnd.nextInt(3)))
+      }
+    }
+
   val points = randomPoints.toList ++ List(
-    Point("Red", rnd.nextInt(160), rnd.nextInt(160), Red, true),
-    Point("Green", rnd.nextInt(160), rnd.nextInt(160), Green, true),
-    Point("Blue", rnd.nextInt(160), rnd.nextInt(160), Blue, true)
+    Point("Red", rnd.nextInt(600), rnd.nextInt(600), Red, true),
+    Point("Green", rnd.nextInt(600), rnd.nextInt(600), Green, true),
+    Point("Blue", rnd.nextInt(600), rnd.nextInt(600), Blue, true)
   )
 
   def mean(values: Seq[Double]): Double =
@@ -74,7 +103,7 @@ object Main extends JSApp {
     (newCentroids, newPoints)
   }
 
-  val SCALE = 4
+  val SCALE = 1
   val SIZE  = 4
 
   def drawPoint(point: Point): G.Graphics[Unit] = point match {
@@ -107,15 +136,15 @@ object Main extends JSApp {
   }
 
   def draw(points: List[Point]): G.Graphics[Unit] = for {
-    _ <- G.clearRect(C.Rectangle(0, 0, 800, 600))
+    _ <- G.clearRect(C.Rectangle(0, 0, 800, MAX))
     _ <- points.map(drawPoint).sequence_
   } yield ()
 
   def loop(points: List[Point], centroids: List[Point]): G.Graphics[Unit] = for {
     _ <- draw(points ++ centroids)
     (newPoints, newCentroids) = kMeans(points ++ centroids)
-    _ <- G.setTimeout(draw(points ++ newCentroids), 1000)
-    _ <- G.setTimeout(loop(newPoints, newCentroids), 2000)
+    _ <- G.setTimeout(draw(points ++ newCentroids), 500)
+    _ <- G.setTimeout(loop(newPoints, newCentroids), 1000)
   } yield ()
 
   def main(): Unit = {
